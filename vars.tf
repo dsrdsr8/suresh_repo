b2a8705cdd62d07f2ee3597292d1195104f5aeb4
dsr.dsr <dsr.dsr8@gmail.com>
2020-04-03 13:53:12 +0530

    Commiting changes vars.tf

--- vars.tf
+++ vars.tf
@@ -1,5 +1,4 @@
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "ap-south-1"
+  access_key = "${aws_access_key}"
+  secret_key = "${aws_secret_key}"
 }
