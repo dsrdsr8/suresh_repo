263b733a7a185d2b3b72ecf839c642771853ec20
dsr.dsr <dsr.dsr8@gmail.com>
2020-04-03 13:46:59 +0530

    Commiting main.tf and vars.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region                  = "ap-south-1"
+  profile                 = "terraform"
+}
+
+
+resource "aws_instance" "myInstance" {
+	ami = "ami-54d2a63b"
+	instance_type="t2.micro"
+
+
+	tags = {
+	Name = "First_Instance_Terraform"
+	}
+
+}
